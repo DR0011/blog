5dc969b66c4713cc21ba67d69ef676809cce945b
Darshan <pateldarshan0011@gmail.com>
2024-05-02 08:33:59 +0530

    create redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/auth.Slice";
+import { Footer } from "./components";
+import { Header } from "./components";
 
 function App() {
-  return (
-    <>
-      <h1>appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
